ab64a5ba4f907a1b7bf38657849d02f29e64d4ec
rajsehar <rajasekhar45@>
2023-09-24 07:43:34 +0530

    kavya loves raj

---
+++ jav.java
@@ -0,0 +1,10 @@
+package rava;
+
+public class jav {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        System.out.println("i lov sree");
+	}
+
+}
---
+++ java.java
@@ -0,0 +1,5 @@
+package rava;
+
+public class java {
+
+}
